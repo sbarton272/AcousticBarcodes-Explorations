62e5dae729458850403d89f03bf1d93a72a85076
Spencer Barton <sbarton272@gmail.com>
2015-03-02 14:38:45 -0500

    Plot transients

--- matlab/transients.m
+++ matlab/transients.m
@@ -1,14 +1,39 @@
 %% Step 1: Transient isolation
+function transients()
 
-clear all
 close all
+clear all
 
-%% Load
-
-FILE = '../
-
-%% Viz
+FILE_START = '../audio/black011001-';
+FILE_END = '.wav';
+FILE_NUMS = 1:9;
+for fileNum = FILE_NUMS
+    fileName = [FILE_START, num2str(fileNum), FILE_END];
+    plotAudio(fileName);
+end
 
 %% Basic filtering
 
 %% Transients
+
+end
+
+function plotAudio(file)
+%% Load
+[y2, Fs] = audioread(file);
+
+% Make single channel
+y1 = sum(y2,2);
+
+%% Viz
+
+sound(y1,Fs);
+
+w = linspace(-1/Fs,1/(2*Fs),length(y1));
+t = linspace(0,length(y1)/Fs,length(y1));
+
+figure;
+subplot(2,1,1); plot(w,fftshift(abs(fft(y1))));
+subplot(2,1,2); plot(t,y1);
+
+end
