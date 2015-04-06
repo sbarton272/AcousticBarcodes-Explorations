c0fe1d7f3d8c09143110d69b0f01702b126199ce
David Lu <davidelu93@gmail.com>
2015-04-05 20:00:11 -0400

    Improve transient detection BY A LOT

--- matlab/transients.m
+++ matlab/transients.m
@@ -47,15 +47,60 @@ lowPass = conv(H,filtered);
 % end
 
 %% Find transient locations
-fltEng = lowPass > std(lowPass);
-riseEdges = diff(fltEng) > 0;
+LOW = 0;
+HIGH = 1;
+FALLING = 2;
+
+LOW_HIGH_THRESH = 0.1 * std(lowPass); % this is arbitrary
+FALL_HIGH_THRESH = 2;
+FALL_LOW_THRESH = LOW_HIGH_THRESH;
+HIGH_FALL_THRESH = 0.3;
+
+state = LOW;
+riseEdges = zeros(size(lowPass));
+isInTransient = false;
+minimum = lowPass(1);
+maximum = 0;
+for i = 1:length(lowPass)
+    n = lowPass(i);
+    switch state
+        case LOW
+            if n > LOW_HIGH_THRESH
+                state = HIGH;
+                riseEdges(i) = true;
+            end
+        case FALLING
+            if n > minimum * FALL_HIGH_THRESH
+                state = HIGH;
+                riseEdges(i) = true;
+            elseif n < FALL_LOW_THRESH
+                state = LOW;
+                minimum = 0;
+            else
+                minimum = min(minimum, n);
+            end
+        case HIGH
+            if n < maximum * HIGH_FALL_THRESH
+                state = FALLING;
+                maximum = 0;
+                minimum = n;
+            else
+                maximum = max(maximum, n);
+            end
+    end
+end
+
+% fltEng = lowPass > std(lowPass);
+% riseEdges = diff(fltEng) > 0;
+
+
 t = 1:length(y)-1;
 
 
 if verbose
-    % figure; plot(t, y(t), t, lowPass(t), t, riseEdges(t)*max(y)); title('Transients');
+    figure; plot(t, y(t), t, lowPass(t), t, riseEdges(t)*max(y)); title('Transients');
     % figure; plot(t, y(t), t, lowPass(t)); title('Transients');
-    figure; plot(t, lowPass(t)); title('Transients');
+    % figure; plot(t, lowPass(t)); title('Transients');
 end
 
 %% Find transients
